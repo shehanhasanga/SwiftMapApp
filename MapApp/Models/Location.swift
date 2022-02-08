215779ddb0cdf1fe0d8d202e9c5ac2bbe5afae79
shehanhasanga <shehanhasanga.15@cse.mrt.ac.lk>
2022-02-08 08:22:07 +0530

    add initial changes

---
+++ MapApp/Models/Location.swift
@@ -0,0 +1,18 @@
+//
+//  Location.swift
+//  MapApp
+//
+//  Created by shehan karunarathna on 2022-02-08.
+//
+
+import Foundation
+import MapKit
+
+struct Location{
+    let name :String
+    let cityName : String
+    let coordinates : CLLocationCoordinate2D
+    let description :String
+    let imageNames :[String]
+    let link : String
+}
