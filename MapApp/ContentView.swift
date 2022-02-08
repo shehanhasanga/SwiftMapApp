0ea1396caf50d0a3bcabfa8dfe7033fd38acdad1
shehanhasanga <shehanhasanga.15@cse.mrt.ac.lk>
2022-02-08 08:07:22 +0530

    Initial Commit

---
+++ MapApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MapApp
+//
+//  Created by shehan karunarathna on 2022-02-08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MapApp/MapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapAppApp.swift
+//  MapApp
+//
+//  Created by shehan karunarathna on 2022-02-08.
+//
+
+import SwiftUI
+
+@main
+struct MapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
